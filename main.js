79fc3ad9547836f7a9b426fd5c1b21906a6aae83
ayalmi25-meet <ayalmiller@gmail.com>
2024-07-16 18:29:07 +0300

    hello

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function changeBackgroundColor1(color) {
+
+    // if (document.getElementById("button").value == 'On')
+		var body = document.getElementById("body")
+		body.style.backgroundColor=color
+
+}
